66df00309471e3f8744cfc3e31e708702e22643a
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-07 04:18:31 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,9 +62,9 @@ def display_form1():
 
     form1.image('MNIST.png', caption="Modified National Institute of Standards and Technology", use_column_width=True)
     text = """MNIST is a large database of handwritten digits that is commonly used for training and
-    testing various image processing systems1234. The acronym stands for Modified National Institute 
-    of Standards and Technology23. MNIST is a popular dataset in the field of machine learning and 
-    can provide a baseline for benchmarking algorithms"""
+    testing various image processing systems. The acronym stands for Modified National Institute 
+    of Standards and Technology. MNIST is a popular dataset in the field of machine learning and 
+    can provide a baseline for benchmarking algorithms."""
     form1.write(text)
   
     submit1 = form1.form_submit_button("Start")
